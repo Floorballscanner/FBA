f5f5657ee48786b6c256a1325536967396776dc3
Mikko <mikkoalanko@hotmail.com>
2021-12-06 21:30:25 +0200

    Added the whitenoise for statics 6.12.2021

--- FBAnalyzer/analyzer/settings.py
+++ FBAnalyzer/analyzer/settings.py
@@ -19,7 +19,7 @@ import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/styles/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'styles'),
     )
