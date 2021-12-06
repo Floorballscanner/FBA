7f1b7e09a786702714bf238ab2afe0becb6bbe48
Mikko <mikkoalanko@hotmail.com>
2021-12-06 19:31:57 +0200

    Changed the namespace again 6.12.2021

--- FBAnalyzer/analyzer/settings.py
+++ FBAnalyzer/analyzer/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'analyzer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
