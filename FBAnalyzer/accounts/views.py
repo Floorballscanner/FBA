5ce8cbfca36c342b3266b1b26350e4a786287c2c
Mikko <mikkoalanko@hotmail.com>
2022-01-01 17:48:35 +0200

    UI development 1.1.2022

--- FBAnalyzer/accounts/views.py
+++ FBAnalyzer/accounts/views.py
@@ -4,11 +4,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .models import Player
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from accounts.forms import AddNewPlayer
 
 @login_required
@@ -25,6 +26,11 @@ def sign_up(request):
             return render(request,'accounts/index.html')
     context['form']=form
     return render(request,'registration/sign_up.html',context)
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/sign_up.html"
 
 def new_game(request):
     return render(request, 'accounts/newgame.html')
