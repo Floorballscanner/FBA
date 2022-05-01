edb60bc313f2fcd8d0c7580079f12e2f3c203a3b
Mikko <mikkoalanko@hotmail.com>
2022-05-01 18:52:24 +0300

    1.5.2022 Added automatic team name change

--- FBAnalyzer/static/js/gamefunctions.js
+++ FBAnalyzer/static/js/gamefunctions.js
@@ -273,14 +273,17 @@
                 started = 1;
                 sData.style.display = "block";
 
+                console.log(1);
                 if (document.getElementById("home_team").value !== "") {
                     name_t1 = document.getElementById("home_team").value;
                     set_t1_names();
+                    console.log(name_t1);
                 }
 
                 if (document.getElementById("away_team").value !== "") {
                     name_t2 = document.getElementById("away_team").value;
                     set_t2_names();
+                    console.log(name_t2);
                 }
 
             } else {
