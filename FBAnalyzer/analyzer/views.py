11ebaef595dd8d0008acf2b0a48db927e1896d56
Mikko <mikkoalanko@hotmail.com>
2022-06-02 22:25:35 +0300

    2.6.2022 Added the signup form functionality

--- FBAnalyzer/analyzer/views.py
+++ FBAnalyzer/analyzer/views.py
@@ -34,7 +34,7 @@ def signup(request):
                 send_mail(subject, message, 'floorballscanner@gmail.com', ['floorballscanner@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
-            return redirect("sign-up")
+            return redirect("frontpage")
 
     form = ContactForm(initial={'message': 'I want to know more about Floorball Scanner, please send me more information.'})
     return render(request, 'sign_up.html', {'form': form})
