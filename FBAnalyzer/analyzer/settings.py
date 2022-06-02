5a07c6aa4928f08b1a40b7ed593c020aac8bd3e6
Mikko <mikkoalanko@hotmail.com>
2022-06-02 21:59:02 +0300

    2.6.2022 Added the signup form functionality

--- FBAnalyzer/analyzer/settings.py
+++ FBAnalyzer/analyzer/settings.py
@@ -143,3 +143,8 @@ options = DATABASES['default'].get('OPTIONS', {})
 options.pop('sslmode', None)
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'floorballscanner@gmail.com'
+EMAIL_HOST_PASSWORD = 'Fl00rballScanner'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
