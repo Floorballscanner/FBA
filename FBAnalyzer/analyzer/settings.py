56190e8283843376588ce3cfbce805e6be1dc520
Mikko <mikkoalanko@hotmail.com>
2022-06-05 21:25:15 +0300

    5.6.2022 Added the Django REST framework

--- FBAnalyzer/analyzer/settings.py
+++ FBAnalyzer/analyzer/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,11 @@ django_heroku.settings(locals())
 # This is new
 options = DATABASES['default'].get('OPTIONS', {})
 options.pop('sslmode', None)
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
