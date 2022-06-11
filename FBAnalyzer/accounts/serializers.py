9867c2b02d0ca1902cf3ec11220857535e69062e
Mikko <mikkoalanko@hotmail.com>
2022-06-11 23:39:28 +0300

    11.6.2022 Live Data Models

--- FBAnalyzer/accounts/serializers.py
+++ FBAnalyzer/accounts/serializers.py
@@ -1,8 +1,8 @@
 
-from rest_framework_json_api import serializers
+from rest_framework import serializers
 from .models import Live
 
 class LiveSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Live
-        fields = ('data')
+        fields = 'data'
