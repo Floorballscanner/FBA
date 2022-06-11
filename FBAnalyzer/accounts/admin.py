5e339ebbed4c792c2dacdcd883a1e81cbd793f67
Mikko <mikkoalanko@hotmail.com>
2022-06-11 19:33:48 +0300

    11.6.2022 Live Data Models

--- FBAnalyzer/accounts/admin.py
+++ FBAnalyzer/accounts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Player
+from .models import Player, Live, Team, Game, Line
 # Register your models here.
 
 admin.site.register(Player)
+admin.site.register(Live)
+admin.site.register(Team)
+admin.site.register(Game)
+admin.site.register(Line)
