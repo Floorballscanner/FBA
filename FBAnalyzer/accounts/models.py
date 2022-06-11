74f9d6b25f14ea0f41bf2ac02e5927487ea00bdc
Mikko <mikkoalanko@hotmail.com>
2022-06-11 19:26:05 +0300

    11.6.2022 Live Data Models

--- FBAnalyzer/accounts/models.py
+++ FBAnalyzer/accounts/models.py
@@ -14,3 +14,49 @@ class Player(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name} ({self.first_name})'
+
+class Live(models.Model):
+    data = models.JSONField()
+
+    def __str__(self):
+        return self.task
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    lineOn = models.IntegerField()
+    possessionPeriod = models.IntegerField()
+    possessionGame = models.IntegerField()
+    goalsPeriod = models.IntegerField()
+    goalsGame = models.IntegerField()
+    xgPeriod = models.DecimalField(max_digits=5, decimal_places=2)
+    xgGame = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.task
+
+class Game(models.Model):
+    date = models.DateTimeField(auto_now = True, blank = True)
+    periodNr = models.IntegerField()
+    gameClock = models.IntegerField()
+    periodClock = models.IntegerField()
+
+    def __str__(self):
+        return self.task
+
+class Line(models.Model):
+    name = models.CharField(max_length=20)
+    possessionPeriod = models.IntegerField()
+    possessionGame = models.IntegerField()
+    gfPeriod = models.IntegerField()
+    gfGame = models.IntegerField()
+    gaPeriod = models.IntegerField()
+    gaGame = models.IntegerField()
+    TOCPeriod = models.IntegerField()
+    TOCGame = models.IntegerField()
+    xGfPeriod = models.DecimalField(max_digits=5, decimal_places=2)
+    xGfGame = models.DecimalField(max_digits=5, decimal_places=2)
+    xGaPeriod = models.DecimalField(max_digits=5, decimal_places=2)
+    xGaGame = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.task
