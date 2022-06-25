dffcc0a343bd9100f2bbb5798a9e5dcc4fbe4a4c
Mikko <mikkoalanko@hotmail.com>
2022-06-25 22:49:26 +0300

    25.6.2022 Live Data Models

--- FBAnalyzer/accounts/models.py
+++ FBAnalyzer/accounts/models.py
@@ -241,7 +241,7 @@ class Game(models.Model):
     xGaGameT2L7 = models.DecimalField(max_digits=5, decimal_places=2, null=True)
 
     def __str__(self):
-        return self.date
+        return f'{self.nameT1} - {self.nameT2}'
 
 class Line(models.Model):
     name = models.CharField(max_length=20)
