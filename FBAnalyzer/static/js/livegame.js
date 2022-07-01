c618d8d3f26c5b0a5941e501dafd2a9ba43d614d
Mikko <mikkoalanko@hotmail.com>
2022-07-01 15:06:15 +0300

    29.6.2022 Live Game Page

--- FBAnalyzer/static/js/livegame.js
+++ FBAnalyzer/static/js/livegame.js
@@ -160,3 +160,25 @@ function convertPos(lPos, counter) {
     return res;
 }
 
+function updateCharts(jsonData) {
+
+    // xG Game Chart
+    var data = google.visualization.arrayToDataTable([
+         ['Line', jsonData.nameT1, { role: 'style' }, { role: 'annotation' } jsonData.nameT2, { role: 'style' }, { role: 'annotation' } ],
+         ['Line 1', jsonData.xGfGameT1L1, 'color: #3046FB', jsonData.xGfGameT1L1, jsonData.xGfGameT2L1, 'color: #59D9EB', jsonData.xGfGameT2L1 ],
+         ['Line 2', jsonData.xGfGameT1L2, 'color: #3046FB', jsonData.xGfGameT1L2, jsonData.xGfGameT2L2, 'color: #59D9EB', jsonData.xGfGameT2L2 ],
+         ['Line 3', jsonData.xGfGameT1L3, 'color: #3046FB', jsonData.xGfGameT1L3, jsonData.xGfGameT2L3, 'color: #59D9EB', jsonData.xGfGameT2L3 ],
+         ['Powerplay', jsonData.xGfGameT1L4 + jsonData.xGfGameT1L5, 'color: #3046FB', jsonData.xGfGameT1L4 + jsonData.xGfGameT1L5, jsonData.xGfGameT2L4 + jsonData.xGfGameT2L5 , 'color: #59D9EB', jsonData.xGfGameT2L4 + jsonData.xGfGameT2L5 ],
+         ['Shorthanded', jsonData.xGfGameT1L6 + jsonData.xGfGameT1L7, 'color: #3046FB', jsonData.xGfGameT1L6 + jsonData.xGfGameT1L7, jsonData.xGfGameT2L6 + jsonData.xGfGameT2L7 , 'color: #59D9EB', jsonData.xGfGameT2L6 + jsonData.xGfGameT2L7 ]
+      ]);
+
+    var options = {
+        title: 'xG by Line',
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+        };
+
+    var chart = new google.visualization.Bar(document.getElementById('xGGame_chart'));
+    chart.draw(data, options);
+
+}
