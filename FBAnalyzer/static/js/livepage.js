0efbdfcdd91278c54ac93f7579d12b3b9f649dc4
Mikko <mikkoalanko@hotmail.com>
2022-07-04 22:29:16 +0300

    3.7.2022 Live Game Page

--- FBAnalyzer/static/js/livepage.js
+++ FBAnalyzer/static/js/livepage.js
@@ -82,7 +82,7 @@ window.onload = function() {
           console.error('Error:', error);
     });
 
-    t = setTimeout(function(){ updatePage() }, 1000);
+    t = setTimeout(function(){ updatePage() }, 10000);
 }
 
 function getCookie(name) {
