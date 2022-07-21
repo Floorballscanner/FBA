602d5e8b7afaf6243fa421439077ffe8023f931a
Mikko <mikkoalanko@hotmail.com>
2022-07-22 00:22:06 +0300

    21.7.2022 Team roster selections

--- FBAnalyzer/static/js/premiumfunctions.js
+++ FBAnalyzer/static/js/premiumfunctions.js
@@ -2092,11 +2092,11 @@
                 removeOptions(s_T1);
                 removeOptions(s_T2);
 
-                console.log(data[i].url)
-                console.log(data[i].name)
+                console.log(data(i).url)
+                console.log(data(i).name)
 
                 for (let i=0; i<=data.length; i++) {
-                    var option = new Option(data[i].url, data[i].name);
+                    var option = new Option(data(i).url, data(i).name);
                     s_T1.appendChild(option);
                     s_T2.appendChild(option);
                 }
