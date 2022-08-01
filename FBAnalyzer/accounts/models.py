84a218e17f3d12507b075360e3fd4f6e204eda83
Mikko <mikkoalanko@hotmail.com>
2022-08-01 23:35:12 +0300

    1.8.2022 Shot model implementation

--- FBAnalyzer/accounts/models.py
+++ FBAnalyzer/accounts/models.py
@@ -106,7 +106,7 @@ class Shot(models.Model):
     isSH = models.BooleanField()
 
     def __str__(self):
-        return f'Time: {self.time}, Shot type: {self.type}, Shot result: {self.result}, xG: {self.xG}'
+        return f'Game: {self.game} Time: {self.time}, Shot type: {self.type}, Shot result: {self.result}, xG: {self.xG}'
 
 class Time(models.Model):
     objects = models.Manager()
