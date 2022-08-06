3d91b5eb65651679a8e2a12ca0c22c9608844cb0
Mikko <mikkoalanko@hotmail.com>
2022-08-06 16:53:15 +0300

    6.8.2022 Data editing functionality

--- FBAnalyzer/static/js/editfunctions.js
+++ FBAnalyzer/static/js/editfunctions.js
@@ -2,34 +2,58 @@
 function editLevel() {
 
     s_level = document.getElementById("edit-level");
+    e_level_name = document.getElementById("edit_level_name");
+    e_level_delete = document.getElementById("edit_level_delete");
+    e_level_country = document.getElementById("edit_level_country");
+    e_level_isSenior = document.getElementById("edit_level_isSenior");
+    e_level_isMale = document.getElementById("edit_level_isMale");
+    e_level_isNational = document.getElementById("edit_level_isNational");
 
     // If user wants to create a new level
 
     if (s_level.options[s_level.selectedIndex].value == "new_level") {
 
-
+            e_level.name.disabled = false;
+            e_level.country.disabled = false;
+            e_level_isSenior.disabled = false;
+            e_level_isMale.disabled = false;
+            e_level_isNational.disabled = false;
+            e_level_delete.disabled = true;
     }
 
     // If selected value exists, fetch data for editing
 
     else {
 
-       /* fetch("https://fbscanner.io/apis/teamlist/?level_id=" + s_Level_T1.options[s_Level_T1.selectedIndex].value)
+       fetch("https://fbscanner.io/apis/teamlist/?level_id=" + s_level.options[s_level.selectedIndex].value)
             .then(response => response.json())
             .then(data => {
                 console.log('Success:', data);
-
-                removeOptions(s_T1);
-
-                for (let i=0; i<data.length; i++) {
-                    var opt = new Option(data[i].name, data[i].id);
-                    s_T1.appendChild(opt);
-                }
+                e_level_name.value = data.name;
+                e_level_country.value = data.country;
+                e_level_isSenior.value = data.isSenior;
+                e_level_isMale.value = data.isMale;
+                e_level_isNational.value = data.isNational;
+                e_level.name.disabled = false;
+                e_level.country.disabled = false;
+                e_level_isSenior.disabled = false;
+                e_level_isMale.disabled = false;
+                e_level_isNational.disabled = false;
+                e_level_delete.disabled = false;
 
         })
             .catch((error) => {
                 console.error('Error:', error);
-        });*/
+        });
 
     }
+}
+
+function editLevelButton() {
+
+
+}
+
+function deleteLevelButton() {
+
 }
