92a0885013ec0cf9e2f26802406655f32d981e86
Mikko <mikkoalanko@hotmail.com>
2022-08-07 16:10:59 +0300

    7.8.2022 Data editing functionality

--- FBAnalyzer/static/js/editfunctions.js
+++ FBAnalyzer/static/js/editfunctions.js
@@ -70,10 +70,6 @@ function editLevelButton() {
     var r = confirm("Do you want to save data?");
     if (r == true) {
 
-        isS = e_level.isSenior.checked;
-        isM = e_level_isMale.checked;
-        isN = e_level_isNational.checked;
-
         data = {
             "name": e_level_name.value,
             "country": e_level_country.value,
@@ -193,20 +189,3 @@ function deleteLevelButton() {
     }
 
 }
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-const csrftoken = getCookie('csrftoken');
