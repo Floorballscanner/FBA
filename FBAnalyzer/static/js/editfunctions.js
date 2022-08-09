0cf021ba9c6975a0919aa89b62c89d934184bf3a
Mikko <mikkoalanko@hotmail.com>
2022-08-09 22:06:03 +0300

    9.8.2022 Data editing functionality

--- FBAnalyzer/static/js/editfunctions.js
+++ FBAnalyzer/static/js/editfunctions.js
@@ -12,6 +12,7 @@ function selectLevel() {
     e_team_name.value = "";
     e_team_id.value = "";
     e_team_level.selectedIndex = "0";
+    e_team.selectedIndex = "0";
     e_team_isMale.checked = false
     e_team_isSenior.checked = false
     e_team_isNational.checked = false
