7e21b46449cb4386097e10255365cb7f1d623645
Mikko <mikkoalanko@hotmail.com>
2022-08-27 20:10:10 +0300

    27.8.2022 Website update

--- FBAnalyzer/analyzer/settings.py
+++ FBAnalyzer/analyzer/settings.py
@@ -166,7 +166,6 @@ CORS_ORIGIN_WHITELIST = (
     'http://192.168.0.50:3000',  # for network
     'http://localhost:8080',  # for localhost (Developlemt)
     'http://192.168.0.50:8080',  # for network (Development)
-    'https://fbscanner.io/livedata',
     'https://*.fbscanner.io'
 )
 
