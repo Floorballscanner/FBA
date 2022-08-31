83e18c70257d2b24ad399f955a530c31d680f753
Pauno Kajoksinen <pauno.kajoksinen@gmail.com>
2022-08-31 17:50:35 +0300

    31.8.2022 New visualizations (T1_line)

--- FBAnalyzer/static/js/premiumfunctions.js
+++ FBAnalyzer/static/js/premiumfunctions.js
@@ -2668,7 +2668,7 @@
         pldata1.addColumn('number', 'Pos%');
 
         pldata1.addRows([
-            ['Line 1', Toc_g[0].value, xf_g[0].value,xa_g[0].value,gf_g[0].value, ga_g[0].value, sf_g[0].value, sa_g[0].value, p_g[0].value],
+            ['Line 1', Toc_g[0], xGf_L1g ,xa_g[0],gf_g[0], ga_g[0], sf_g[0], sa_g[0], p_g[0]],
             ['Line 2', Toc_g[1], xf_g[1],xa_g[1],gf_g[1], ga_g[1], sf_g[1], sa_g[1], p_g[1]],
             ['Line 3', Toc_g[2], xf_g[2],xa_g[2],gf_g[2], ga_g[2], sf_g[2], sa_g[2], p_g[2]],
             ['Powerplay', Toc_g[3], xf_g[3],xa_g[3],gf_g[3], ga_g[3], sf_g[3], sa_g[3], p_g[3]],
