87177eb45d420ac9922a5427bf6a6b78c13b8a67
Pauno Kajoksinen <pauno.kajoksinen@gmail.com>
2022-08-31 20:34:07 +0300

    31.8.2022 New visualizations (T1_line)

--- FBAnalyzer/static/js/premiumfunctions.js
+++ FBAnalyzer/static/js/premiumfunctions.js
@@ -2663,12 +2663,12 @@
         pldata1.addColumn('number', 'xGoals agains');
         pldata1.addColumn('number', 'Plus');
         pldata1.addColumn('number', 'Minus');
-        pldata1.addColumn('string', 'Corsi For');
+        pldata1.addColumn('number', 'Corsi For');
         pldata1.addColumn('number', 'Corsi Against');
         pldata1.addColumn('number', 'Posession%');
 
         pldata1.addRows([
-            ['Line 1', Toc_g[0].value, Number(xf_g[0]), stT1Teamg_array[0], gf_g[0], ga_g[0], sf_g[0], sa_g[0], p_g[0]],
+            ['Line 1', Toc_g[0].value, Number(xf_g[0]), stT1Teamg_array[0], gf_g[0], ga_g[0], sf_g[0], 0.82, p_g[0]],
             ['Line 2', Toc_g[1].value, xf_g[1],xa_g[1],gf_g[1], ga_g[1], sf_g[1], sa_g[1], p_g[1]],
             ['Line 3', Toc_g[2].value, xf_g[2],xa_g[2],gf_g[2], ga_g[2], sf_g[2], sa_g[2], p_g[2]],
             ['Powerplay', Toc_g[3].value, xf_g[3],xa_g[3],gf_g[3], ga_g[3], sf_g[3], sa_g[3], p_g[3]],
