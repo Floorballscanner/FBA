c76f29b76a2fd29b0f4b9e72b7dd0942972f360b
Mikko <mikkoalanko@hotmail.com>
2022-10-16 23:18:12 +0300

    16.10.2022 Undo functionality

--- FBAnalyzer/static/js/premiumfunctions.js
+++ FBAnalyzer/static/js/premiumfunctions.js
@@ -3879,21 +3879,7 @@
     }
     function undoButton() {
 
-        console.log("Picture now equals undo picture: " + cnvs.toDataURL() == undo_object.cnvs_url);
-        myImg.src = undo_object.cnvs_url;
         var ctx = cnvs.getContext("2d");
-        ctx.drawImage(myImg,0,0,fWidth,fLength);
-        myImg.src = undo_object.cnvs_1_url;
-        var ctx1 = cnvs_1.getContext("2d");
-        ctx1.drawImage(myImg,0,0,fWidth,fLength);
-        myImg.src = undo_object.cnvs_url;
-        var ctx2 = cnvs_2.getContext("2d");
-        ctx2.drawImage(myImg,0,0,fWidth,fLength);
-        myImg.src = undo_object.cnvs_3_url;
-        var ctx3 = cnvs_3.getContext("2d");
-        ctx3.drawImage(myImg,0,0,fWidth,fLength);
-        myImg.src = undo_object.cnvs_4_url;
-        var ctx4 = cnvs_4.getContext("2d");
-        ctx4.drawImage(myImg,0,0,fWidth,fLength);
+        ctx.clearRect(0,0,fWidth,fLength);
         document.getElementById("undo").disabled = true;
     }
