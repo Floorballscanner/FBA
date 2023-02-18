7e0a75afcd981ca4d1ac49d11c4ff955b41d4ce4
Mikko <floorballscanner@gmail.com>
2023-02-18 19:07:45 +0200

    18.2.2023 Added fliiga.html

--- FBAnalyzer/static/js/fliigadata.js
+++ FBAnalyzer/static/js/fliigadata.js
@@ -44,7 +44,7 @@ function getGameData(selectObject) {
             'Origin': 'http://www.tilastopalvelu.fi',
             'Referer': 'http://www.tilastopalvelu.fi/fb/gameshootingmap/?gid=' + gameID + '&lang=',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-            'X-Requested-With': 'XMLHttpsRequest'
+            'X-Requested-With': 'XMLHttpRequest'
         },
         body: new URLSearchParams({
             'GameID': gameID
