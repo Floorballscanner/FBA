acee2efa4dec14906aa83246daa386ca69bb1f3f
Mikko <floorballscanner@gmail.com>
2023-02-18 19:00:10 +0200

    18.2.2023 Added fliiga.html

--- FBAnalyzer/static/js/fliigadata.js
+++ FBAnalyzer/static/js/fliigadata.js
@@ -48,94 +48,93 @@ function getGameData(selectObject) {
         },
         body: new URLSearchParams({
             'GameID': gameID
-        })
+        })})
+
         .then(response => response.json())
-          .then(temp => {
+        .then(temp => {
 
-            let shots_temp = temp.map(s => ({
-              UniqueID: s.UniqueID,
-              ShotX: s.ShotX,
-              ShotY: s.ShotY,
-              GoalX: s.GoalX,
-              GoalY: s.GoalY,
-              PlayerID: s.PlayerID,
-              Period: s.Period,
-              GameTime: s.GameTime,
-              LogTime: s.LogTime,
-              EventType: s.EventType,
-              LTeamID: s.LTeamID,
-              RTeamID: s.RTeamID,
-              ShootingTeamID: s.ShootingTeamID,
-              ShootingPlayerID: s.ShootingPlayerID,
-              BlockingPlayerID: s.BlockingPlayerID,
-              Plus: s.Plus,
-              Minus: s.Minus,
-              Ass1Jersey: s.Ass1Jersey,
-              Ass2Jersey: s.Ass2Jersey,
-              Goaltype: s.Goaltype,
+        let shots_temp = temp.map(s => ({
+          UniqueID: s.UniqueID,
+          ShotX: s.ShotX,
+          ShotY: s.ShotY,
+          GoalX: s.GoalX,
+          GoalY: s.GoalY,
+          PlayerID: s.PlayerID,
+          Period: s.Period,
+          GameTime: s.GameTime,
+          LogTime: s.LogTime,
+          EventType: s.EventType,
+          LTeamID: s.LTeamID,
+          RTeamID: s.RTeamID,
+          ShootingTeamID: s.ShootingTeamID,
+          ShootingPlayerID: s.ShootingPlayerID,
+          BlockingPlayerID: s.BlockingPlayerID,
+          Plus: s.Plus,
+          Minus: s.Minus,
+          Ass1Jersey: s.Ass1Jersey,
+          Ass2Jersey: s.Ass2Jersey,
+          Goaltype: s.Goaltype,
 
-            }));
+        }));
 
-            let shots = new DataFrame(shots_temp);
-            shots['GameTime'] = shots['GameTime'].apply(parseFloat);
-            shots = shots.sort_values('GameTime');
-            shots['xGOT'] = 0;
-            shots['xG'] = 0;
+        let shots = new DataFrame(shots_temp);
+        shots['GameTime'] = shots['GameTime'].apply(parseFloat);
+        shots = shots.sort_values('GameTime');
+        shots['xGOT'] = 0;
+        shots['xG'] = 0;
 
-            for (let i = 0; i < shots.length; i++) {
-              let x = parseFloat(shots.loc[i]['ShotX']);
-              if (x === 512) {
-                x = 513;
-              }
-              let y = parseFloat(shots.loc[i]['ShotY']);
-              let [xGOT, xG] = calcxG(x, y);
-              if (shots.loc[i]['EventType'] === '1' || shots.loc[i]['EventType'] === '2') {
-                shots.loc[i]['xGOT'] = xGOT;
-              } else {
-                shots.loc[i]['xGOT'] = 0;
-              }
-              shots.loc[i]['xG'] = xG;
-            }
+        for (let i = 0; i < shots.length; i++) {
+          let x = parseFloat(shots.loc[i]['ShotX']);
+          if (x === 512) {
+            x = 513;
+          }
+          let y = parseFloat(shots.loc[i]['ShotY']);
+          let [xGOT, xG] = calcxG(x, y);
+          if (shots.loc[i]['EventType'] === '1' || shots.loc[i]['EventType'] === '2') {
+            shots.loc[i]['xGOT'] = xGOT;
+          } else {
+            shots.loc[i]['xGOT'] = 0;
+          }
+          shots.loc[i]['xG'] = xG;
+        }
 
-            let team1 = shots.filter(s => s['ShootingTeamID'] === team1nr);
-            let team2 = shots.filter(s => s['ShootingTeamID'] === team2nr);
+        let team1 = shots.filter(s => s['ShootingTeamID'] === team1nr);
+        let team2 = shots.filter(s => s['ShootingTeamID'] === team2nr);
 
-            let team1xGot = shots.filter(s => s['ShootingTeamID'] === team1nr && (s['EventType'] === '1' || s['EventType'] === '2'));
-            let team2xGot = shots.filter(s => s['ShootingTeamID'] === team2nr && (s['EventType'] === '1' || s['EventType'] === '2'));
+        let team1xGot = shots.filter(s => s['ShootingTeamID'] === team1nr && (s['EventType'] === '1' || s['EventType'] === '2'));
+        let team2xGot = shots.filter(s => s['ShootingTeamID'] === team2nr && (s['EventType'] === '1' || s['EventType'] === '2'));
 
-            let team1xGmis = shots.filter(s => s['ShootingTeamID'] === team1nr && s['EventType'] === '0');
-            let team2xGmis = shots.filter(s => s['ShootingTeamID'] === team2nr && s['EventType'] === '0');
+        let team1xGmis = shots.filter(s => s['ShootingTeamID'] === team1nr && s['EventType'] === '0');
+        let team2xGmis = shots.filter(s => s['ShootingTeamID'] === team2nr && s['EventType'] === '0');
 
-            let team1saxG = shots.filter(s => s['ShootingTeamID'] === team1nr && s['EventType'] === '2');
-            let team2saxG = shots.filter(s => s['ShootingTeamID'] === team2nr && s['EventType'] === '2');
+        let team1saxG = shots.filter(s => s['ShootingTeamID'] === team1nr && s['EventType'] === '2');
+        let team2saxG = shots.filter(s => s['ShootingTeamID'] === team2nr && s['EventType'] === '2');
 
-            let team1goxG = shots.filter(s => s['ShootingTeamID'] === team1nr && s['EventType'] === '1');
-            let team2goxG = shots.filter(s => s['ShootingTeamID'] === team2nr && s['EventType'] === '1');
+        let team1goxG = shots.filter(s => s['ShootingTeamID'] === team1nr && s['EventType'] === '1');
+        let team2goxG = shots.filter(s => s['ShootingTeamID'] === team2nr && s['EventType'] === '1');
 
-            let team1xG = Math.round(team1.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
-            let team2xG = Math.round(team2.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
+        let team1xG = Math.round(team1.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
+        let team2xG = Math.round(team2.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
 
-            let team1ot = Math.round(team1xGot.reduce((acc, s) => acc + s['xGOT'], 0) * 100) / 100;
-            let team2ot = Math.round(team2xGot.reduce((acc, s) => acc + s['xGOT'], 0) * 100) / 100;
+        let team1ot = Math.round(team1xGot.reduce((acc, s) => acc + s['xGOT'], 0) * 100) / 100;
+        let team2ot = Math.round(team2xGot.reduce((acc, s) => acc + s['xGOT'], 0) * 100) / 100;
 
-            let team1otshots = team1xGot.length;
-            let team2otshots = team2xGot.length;
+        let team1otshots = team1xGot.length;
+        let team2otshots = team2xGot.length;
 
-            let team1mis = Math.round(team1xGmis.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
-            let team2mis = Math.round(team2xGmis.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
+        let team1mis = Math.round(team1xGmis.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
+        let team2mis = Math.round(team2xGmis.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
 
-            let team1sav = Math.round(team1saxG.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
-            let team2sav = Math.round(team2saxG.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
+        let team1sav = Math.round(team1saxG.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
+        let team2sav = Math.round(team2saxG.reduce((acc, s) => acc + s['xG'], 0) * 100) / 100;
 
-            let team1go = team1goxG.length;
-            let team2go = team2goxG.length;
+        let team1go = team1goxG.length;
+        let team2go = team2goxG.length;
 
-            let time = parseInt(shots['GameTime'][shots.length - 1]) / 60;
-            let hours = parseInt(time);
-            let minutes = (time * 60) % 60;
-            let seconds = (time * 3600) % 60;
-
-          });
+        let time = parseInt(shots['GameTime'][shots.length - 1]) / 60;
+        let hours = parseInt(time);
+        let minutes = (time * 60) % 60;
+        let seconds = (time * 3600) % 60;
 
         document.getElementById('fltotg_1').innerHTML = team1go;
         document.getElementById('fltotg_2').innerHTML = team2go;
@@ -144,7 +143,7 @@ function getGameData(selectObject) {
         document.getElementById('flt1name1').innerHTML = team1name;
         document.getElementById('flt1name2').innerHTML = team2name;
         document.getElementById('fldate').innerHTML = '18.2.2023';
-    });
+        });
 }
 
 
