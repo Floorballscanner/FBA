d522a707d9c9f0fd6ab3eb1a52e16774a298cab6
Mikko <floorballscanner@gmail.com>
2023-09-03 12:34:04 +0400

    3.9.2023 New print function

--- FBAnalyzer/static/js/premiumfunctions.js
+++ FBAnalyzer/static/js/premiumfunctions.js
@@ -4147,11 +4147,10 @@
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
                 <script src="https://smtpjs.com/v3/smtp.js"></script>
+                <script> printDraw() </script>
             </body>
             </html>
         `);
-
-        printDraw(); // Draw the print window charts
 
         /*// Close the document after printing
         printWindow.document.close();
