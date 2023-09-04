26f48ce3187ce14937c5ed16cb9c990a0a8bf612
Mikko <floorballscanner@gmail.com>
2023-09-04 15:34:58 +0400

    4.9.2023 New print function

--- FBAnalyzer/static/js/premiumfunctions.js
+++ FBAnalyzer/static/js/premiumfunctions.js
@@ -4105,8 +4105,6 @@
         fnAsync();
 }
 
-
-
         // Add the content to be printed to the new window
         printWindow.document.write(`
             <html>
@@ -4151,14 +4149,12 @@
                     </div>
                     <div class="row" style="display: flex;Padding-top: 20px;">
                         <div class="column" style="flex:50%;">
-                            <img src = ${p_T1_linestats_chart} width="500px">
-                            <br><br>
-                            <img src = ${p_T1_plstats_chart} width="400px"><br>
+                            <img src = ${p_T1_linestats_chart} style="Padding-top: 20px;" width="500px">
+                            <img src = ${p_T1_plstats_chart} style="Padding-top: 20px;" width="400px"><br>
                         </div>
                         <div class="column" style="flex:50%;">
-                            <img src = ${p_T2_linestats_chart} width="500px">
-                            <br><br>
-                            <img src = ${p_T2_plstats_chart} width="400px"><br>
+                            <img src = ${p_T2_linestats_chart} style="Padding-top: 20px;" width="500px">
+                            <img src = ${p_T2_plstats_chart} style="Padding-top: 20px;" width="400px"><br>
                         </div>
                     </div>
                     <br>
