bcbabdea35d6dd5f49d8c5c20a2c9a93375b4e55
Mikko <floorballscanner@gmail.com>
2023-09-26 20:20:12 +0300

    26.9.2023 Test Analysis - testing

---
+++ FBAnalyzer/static/js/test_analysis.js
@@ -0,0 +1,25 @@
+
+var s_game = document.getElementById("select-game");
+
+function changeGame() {
+
+    game_id = s_game.options[s_game.selectedIndex].value;
+
+    fetch("https://fbscanner.io/apis/games/" + game_id + "/")
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    initializePage()
+                    gd = data.game_data;
+                    if (Object.keys(gd).length > 0) { // If game data is not empty
+
+
+
+                    }
+                }
+
+            })
+                .catch((error) => {
+                    console.error('Error:', error);
+            });
+}
