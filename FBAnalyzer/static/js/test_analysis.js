f02540431e95a4e2dde813379f648cac26102b07
Mikko <floorballscanner@gmail.com>
2023-09-26 21:12:45 +0300

    26.9.2023 Test Analysis - testing

--- FBAnalyzer/static/js/test_analysis.js
+++ FBAnalyzer/static/js/test_analysis.js
@@ -12,21 +12,30 @@ function changeGame() {
                     gd = data.game_data;
                     if (Object.keys(gd).length > 0) { // If game data is not empty
 
-                        // Create an empty Set to store unique shooter names
-                        const uniqueShooters = new Set();
+                        // Create an empty array to store shooter objects
+                        const shooterObjects = [];
 
                         // Iterate through the array of arrays and extract shooter names
                         gd.printShotData.forEach(row => {
-                            if (row[1].includes(gd.name_t1)) { // Check the 2nd column for team 1
+                            if (row[1].includes(name_t1)) {
                                 const shooterName = row[9]; // Assuming array indexing is 0-based
-                                uniqueShooters.add(shooterName);
+                                const matches = shooterName.match(/#(\d+) (.+)/); // Extract jersey number and name
+
+                                if (matches && matches.length === 3) {
+                                  const jerseyNumber = parseInt(matches[1], 10);
+                                  const playerName = matches[2];
+                                  shooterObjects.push({ jerseyNumber, playerName });
+                                }
                             }
                         });
 
-                        // Convert the Set to an array if needed
-                        const uniqueShootersArray = Array.from(uniqueShooters);
-                        console.log(uniqueShootersArray);
+                        // Sort shooter objects by jersey number in ascending order
+                        shooterObjects.sort((a, b) => a.jerseyNumber - b.jerseyNumber);
 
+                        // Extract the sorted shooter names from the sorted objects
+                        const sortedShooters = shooterObjects.map(obj => `#${obj.jerseyNumber} ${obj.playerName}`);
+
+                        console.log(sortedShooters);
                     }
                 })
                 .catch((error) => {
