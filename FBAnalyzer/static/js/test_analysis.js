9609a41863edee45900619b77275217f84877ff6
Mikko <floorballscanner@gmail.com>
2023-10-09 23:06:40 +0300

    9.10.2023 Test Analysis mod, x y logic update

--- FBAnalyzer/static/js/test_analysis.js
+++ FBAnalyzer/static/js/test_analysis.js
@@ -291,7 +291,7 @@ function drawMap(pl) {
 
                 [x,y] = calcxy(radius,angle);
                 x = (x * fWidth / 20) + (fWidth/2);
-                y = (y * fLength / 40 + (fLength/10);
+                y = (y * fLength / 40) + (fLength/10);
 
                 ctx1.fillStyle = "blue";
                 ctx1.fillText("o", x, y);
