031b58e240fe6b93e8638e26801f2af8debdf2bf
Mikko <floorballscanner@gmail.com>
2023-11-04 17:44:49 +0200

    4.11.2023 F-liiga results test page

--- FBAnalyzer/static/js/fliigapage.js
+++ FBAnalyzer/static/js/fliigapage.js
@@ -14,7 +14,7 @@ window.onload = function() {
         .then(data => {
             console.log(data)
             const matches_json = data.matches;
-            matches_json.sort(GetSortOrder("date"))
+            matches_json.sort(GetSortOrder("date"));
 
             // List of keys you want to select from matches_json
             const selectedKeys = ['match_id','match_number','category_name','date','time','team_A_id','team_A_name','team_B_id','team_B_name','status'];
@@ -32,74 +32,11 @@ window.onload = function() {
               });
               modifiedMatches.push(modifiedMatch);
             });
+
             matches = modifiedMatches;
-/*            for (let i = 0; i < rows ; i++) {
-
-                const div = document.createElement('div');
-                div.setAttribute('class', 'row');
-
-                const div2 = document.createElement('div');
-                div2.setAttribute('class', 'col-sm-12');
-
-                const d = document.createElement('h5');
-                d.innerText = data[i].date.substr(0, 10);
-                d.style.paddingTop = "50px";
-
-                const h = document.createElement('h1');
-                h.innerText = data[i].nameT1 + " - " + data[i].nameT2;
-                h.style.paddingTop = "5px";
-                h.style.fontWeight = "bold"
-
-                const h2 = document.createElement('h1');
-                h2.innerText = data[i].goalsGameT1 + " - " + data[i].goalsGameT2;
-                h2.style.paddingTop = "5px";
-                h2.setAttribute('id', 'goals' + i);
-
-                const h3 = document.createElement('h5');
-                h3.innerText = "Period " + data[i].periodNr;
-                h3.style.paddingTop = "5px";
-                h3.setAttribute('id', 'period' + i);
-
-                var date = new Date(data[i].periodClock * 1000);
-                var display = date.toISOString().substr(11, 8);
-                const disp = document.createElement('h3');
-                disp.innerText = display;
-                disp.style.paddingTop = "5px";
-                disp.setAttribute('id', 'time' + i);
-
-                const text = data[i].url;
-                const nrArray = text.split("/");
-                const nr = nrArray[nrArray.length-2];
-                const button = document.createElement('a');
-                button.setAttribute('class', 'btn btn-primary');
-                button.setAttribute('href', '/live/' + nr);
-                button.setAttribute('role', 'button');
-                button.style.paddingTop = "5px";
-                button.innerText = "Open live";
-                button.style.paddingBottom = "5px";
-
-                document.getElementById("head").appendChild(div);
-                div.appendChild(div2);
-                div2.appendChild(d);
-
-                if (Date.now() - Date.parse(data[i].date) <= 3600000) { // max 1 hour from last update
-                    const img = document.createElement('img');
-                    img.setAttribute('src',"/static/live.png");
-                    img.setAttribute('width', '70px');
-                    img.style.paddingTop = "5px";
-                    div2.appendChild(img);
-                }
-                //else {h.style.paddingTop = "55px";}
-
-                div2.appendChild(h);
-                div2.appendChild(h2);
-                div2.appendChild(h3);
-                div2.appendChild(disp);
-                div2.appendChild(button);*/
-
-        })
             console.log('Success:', data);
 
+        })
         .catch((error) => {
           console.error('Error:', error);
     });
