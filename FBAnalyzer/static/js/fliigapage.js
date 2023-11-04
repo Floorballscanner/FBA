8ef0b63ffa628be5e427539b5ae30ef3af378e37
Mikko <floorballscanner@gmail.com>
2023-11-04 17:26:22 +0200

    4.11.2023 F-liiga results test page

--- FBAnalyzer/static/js/fliigapage.js
+++ FBAnalyzer/static/js/fliigapage.js
@@ -97,7 +97,7 @@ window.onload = function() {
                 div2.appendChild(disp);
                 div2.appendChild(button);*/
 
-        }
+        })
 
             console.log('Success:', data);
         })
