23bc60310eedc7c2b05bf9eaf6583c38bc34f6c8
Mikko <floorballscanner@gmail.com>
2023-12-16 14:45:02 +0200

    10.12.2023 F-liiga live test jersey color improvement

--- FBAnalyzer/static/js/fliigalivegame.js
+++ FBAnalyzer/static/js/fliigalivegame.js
@@ -25,10 +25,10 @@ var lineup_t1g = [];
 var lineup_t2g = [];
 var shots = [];
 var goaliedata = [];
-var t1color = "red";
-var t2color = "blue";
-var t1color_rgba = 'rgba(255, 0, 0';
-var t2color_rgba = 'rgba(0, 0, 255';
+var t1color = "#990000";
+var t2color = "#002072";
+var t1color_rgba = 'rgba(153, 0, 0';
+var t2color_rgba = 'rgba(0, 32, 114';
 
 var maxY = 3400; // Arvioitu, päätyviiva 0 - keskiviiva 1700
 var maxX = 2000; // [-1000, 1000], maalivahdin näkökulmasta katsottuna oikealle negatiivinen, 0 keskilinjalla
