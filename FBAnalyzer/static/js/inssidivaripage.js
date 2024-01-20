3f8f0093a5c7d246940fce8fe2aaf1ae7e2a1a51
Pauno Kajoksinen <pauno.kajoksinen@gmail.com>
2024-01-20 16:01:45 +0200

    20.1 Tempo & momentumin skaalat

--- FBAnalyzer/static/js/inssidivaripage.js
+++ FBAnalyzer/static/js/inssidivaripage.js
@@ -377,6 +377,13 @@ function drawCharts() {
             1: {color: t2color},
             2: {type: 'line', color: 'black'}
         }
+        vAxis: {
+        textPosition: 'none', // Piilottaa y-akselin labelit
+        viewWindow: {
+            min: mommmin - 0.1, // Käytä muuttujaa minimiarvona
+            max: mommmax + 0.1 // Käytä muuttujaa maksimiarvona
+            }
+        }
     };
 
     var chartmomentum = new google.visualization.ComboChart(document.getElementById('livemomentumchart'));
@@ -396,6 +403,13 @@ function drawCharts() {
             0: {color: 'forestgreen'},
             1: {color: 'red'},
             2: {type: 'line', color: 'black'}
+        }
+        vAxis: {
+        textPosition: 'none', // Piilottaa y-akselin labelit
+        viewWindow: {
+            min: tempmin - 0.1, // Käytä muuttujaa minimiarvona
+            max: tempmax + 0.1 // Käytä muuttujaa maksimiarvona
+            }
         }
     };
 
@@ -695,6 +709,15 @@ function calckello() {
     ]);
     momm.unshift(['Aika', t1name, t2name, 'Even']);
 
+    momax = momm.slice(1).map(function(rivi) {
+        return rivi[1]; // Oletetaan, että "Tempo" on aina toisessa sarakkeessa (indeksi 1)
+    });
+    momin = momm.slice(2).map(function(rivi) {
+        return rivi[1]; // Oletetaan, että "Tempo" on aina toisessa sarakkeessa (indeksi 1)
+    });
+    mommmax = Math.max(...temmax);
+    mommmin = Math.min(...temmin)
+
 
     tempo = Array.from({ length: kello.length }, (_, index) => [
        index + 1,
@@ -703,6 +726,17 @@ function calckello() {
        0
     ]);
     tempo.unshift(['Aika', 'Nice amount of action!','No action', 'Average tempo']);
+
+    temmax = tempo.slice(1).map(function(rivi) {
+        return rivi[1]; // Oletetaan, että "Tempo" on aina toisessa sarakkeessa (indeksi 1)
+    });
+    temmin = tempo.slice(2).map(function(rivi) {
+        return rivi[1]; // Oletetaan, että "Tempo" on aina toisessa sarakkeessa (indeksi 1)
+    });
+    tempmax = Math.max(...temmax);
+    tempmin = Math.min(...temmin);
+
+
    }
 
 function calcDistArray() {
