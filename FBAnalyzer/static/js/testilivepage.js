8e8edc809f5756f468e3261c7e0b3f2571f8ebdd
Mikko <floorballscanner@gmail.com>
2024-03-01 20:58:53 +0200

    1.3.2024 Testing new live page

--- FBAnalyzer/static/js/testilivepage.js
+++ FBAnalyzer/static/js/testilivepage.js
@@ -27,16 +27,16 @@ window.onload = function() {
                     row.setAttribute('class', 'row w-100');
 
                     const div1 = document.createElement('div');
-                    div1.setAttribute('class', 'col-xs-1 px-1 col-auto');
+                    div1.setAttribute('class', 'col-xs-1 px-1');
 
                     const div2 = document.createElement('div');
-                    div2.setAttribute('class', 'col-xs-9 px-0 col-auto');
+                    div2.setAttribute('class', 'col-xs-9 px-0');
 
                     const div3 = document.createElement('div');
-                    div3.setAttribute('class', 'col-xs-1 px-1 col-auto');
+                    div3.setAttribute('class', 'col-xs-1 px-1');
 
                     const div4 = document.createElement('div');
-                    div4.setAttribute('class', 'col-xs-1 px-1 col-auto');
+                    div4.setAttribute('class', 'col-xs-1 px-1');
 
                     const imgcat = document.createElement('img');
                     imgcat.setAttribute('src', match.category_logo);
