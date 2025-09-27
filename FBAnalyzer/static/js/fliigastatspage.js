fcf219764074d94a1c91ad7519601d53b9d25aef
Mikko <floorballscanner@gmail.com>
2025-09-27 21:04:58 +0300

    27.9.2025. New Fliiga Statistics page development started

--- FBAnalyzer/static/js/fliigastatspage.js
+++ FBAnalyzer/static/js/fliigastatspage.js
@@ -277,108 +277,12 @@ function selectMen() {
 
 function selectWomen() {
 
-    s_game.innerHTML = "";
-    opt = new Option("Select a game...");
-    s_game.appendChild(opt);
+    api_key = 'n76qrhjnyygtcz7fzhg57sftbv6wtgjk';
+    comp_id = 'sb2025';
+    cat_id = '384';
+    group_id = '1';
 
-    fetch("https://salibandy.api.torneopal.com/taso/rest/getMatches?api_key="+api_key+"&season_id=2025-2026&competition_id=sb2025&category_id=384&group_id=1")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            matches_json = data.matches;
-
-/*            fetch("https://salibandy.api.torneopal.com/taso/rest/getMatches?api_key="+api_key+"&season_id=2024-2025&competition_id=sb2024&category_id=384&group_id=2")
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log(data)
-                        matches_json = matches_json.concat(data.matches);*/
-
-                        matches_json.sort(GetSortOrder("date"));
-
-                        // List of keys you want to select from matches_json
-                        const selectedKeys = ['match_id','match_number','category_name','date','time','team_A_id','team_A_name','team_B_id','team_B_name','status'];
-
-                        // Create a new array to store the modified JSON objects
-                        const modifiedMatches = [];
-
-                        // Iterate through matches_json and create new objects with selected keys
-                        matches_json.forEach(match => {
-                          const modifiedMatch = {};
-                          selectedKeys.forEach(key => {
-                            if (match.hasOwnProperty(key)) {
-                              modifiedMatch[key] = match[key];
-                            }
-                          });
-                          modifiedMatches.push(modifiedMatch);
-                        });
-
-                        matches = modifiedMatches;
-
-                        for (let j=0; j<matches.length; j++) {
-                            if (matches[j].status == 'Played') {
-                                opt = new Option(matches[j].date + " | "  + matches[j].team_A_name + " - " + matches[j].team_B_name, matches[j].match_id);
-                                s_game.appendChild(opt);
-                            }
-                        }
-                        console.log('Success:', data);
-
-/*                        })
-
-                    .catch((error) => {
-                          console.error('Error:', error);
-                    });*/
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-    });
-}
-
-function selectID() {
-
-    s_game.innerHTML = "";
-    opt = new Option("Select a game...");
-    s_game.appendChild(opt);
-
-    fetch("https://salibandy.api.torneopal.com/taso/rest/getMatches?api_key="+api_key+"&season_id=2025-2026&competition_id=sb2025&category_id=444&group_id=1")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            matches_json = data.matches;
-
-            matches_json.sort(GetSortOrder("date"));
-
-            // List of keys you want to select from matches_json
-            const selectedKeys = ['match_id','match_number','category_name','date','time','team_A_id','team_A_name','team_B_id','team_B_name','status'];
-
-            // Create a new array to store the modified JSON objects
-            const modifiedMatches = [];
-
-            // Iterate through matches_json and create new objects with selected keys
-            matches_json.forEach(match => {
-              const modifiedMatch = {};
-              selectedKeys.forEach(key => {
-                if (match.hasOwnProperty(key)) {
-                  modifiedMatch[key] = match[key];
-                }
-              });
-              modifiedMatches.push(modifiedMatch);
-            });
-
-            matches = modifiedMatches;
-
-            for (let j=0; j<matches.length; j++) {
-                if (matches[j].status == 'Played') {
-                    opt = new Option(matches[j].date + " | "  + matches[j].team_A_name + " - " + matches[j].team_B_name, matches[j].match_id);
-                    s_game.appendChild(opt);
-                }
-            }
-            console.log('Success:', data);
-
-            })
-
-        .catch((error) => {
-              console.error('Error:', error);
-        });
+    main().catch(err => console.error(err));
 
 }
 
